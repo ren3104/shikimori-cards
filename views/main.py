c75ac4e234837057e66ef07517dba3b016ab25b3
ren3104 <2ren3104@gmail.com>
2023-06-09 08:47:32 +0300

    Update redirect url for index route

--- views/main.py
+++ views/main.py
@@ -6,4 +6,4 @@ bp_main = Blueprint("main", __name__)
 
 @bp_main.route("/")
 def index():
-    return redirect("https://github.com/ren3104")
+    return redirect("https://github.com/ren3104/shikimori-cards")
