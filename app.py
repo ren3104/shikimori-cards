d6be2df9d1930bce014e9f9206e2aa69c9e0cbca
ren3104 <2ren3104@gmail.com>
2024-01-04 10:08:22 +0300

    Fix

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ from views.cards import bp_cards
 
 uvloop.install()
 
-wsgi_app = Flask(__name__)
-wsgi_app.register_blueprint(bp_main)
-wsgi_app.register_blueprint(bp_cards)
+app = Flask(__name__)
+app.register_blueprint(bp_main)
+app.register_blueprint(bp_cards)
 
 
-@wsgi_app.teardown_appcontext
+@app.teardown_appcontext
 async def teardown_web_sessions(_: Optional[Exception]) -> None:
     try:
         await cast(ClientSession, g.aiohttp_session).close()
