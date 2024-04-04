968828011db50e84982a103186dcd8c3f9034859
ren3104 <2ren3104@gmail.com>
2024-04-04 22:24:30 +0300

    Wipe

--- src/fetchers/collection_fetcher.py
+++ src/fetchers/collection_fetcher.py
@@ -19,28 +19,27 @@ async def fetch_collection_card(
     client: ClientSession,
     collection_id: int
 ) -> CollectionCard:
-    async with client:
-        async with client.request(
-            method="GET",
-            url=f"https://shikimori.one/collections/{collection_id}"
-        ) as response:
+    async with client.request(
+        method="GET",
+        url=f"https://shikimori.one/collections/{collection_id}"
+    ) as response:
 
-            html_tree = HTMLParser(await response.text())
-            status_line_html = html_tree.css_first(".l-page .inner .b-status-line")
-            votes_html = status_line_html.css_first(".critique-votes_count")
+        html_tree = HTMLParser(await response.text())
+        status_line_html = html_tree.css_first(".l-page .inner .b-status-line")
+        votes_html = status_line_html.css_first(".critique-votes_count")
 
-            changed_at = None
-            changed_at_html = status_line_html.css_first(".changed_at time")
-            if changed_at_html is None:
-                changed_at = status_line_html.css_first(".created_at time").attrs.get("datetime")
-            else:
-                changed_at = changed_at_html.attrs.get("datetime")
+        changed_at = None
+        changed_at_html = status_line_html.css_first(".changed_at time")
+        if changed_at_html is None:
+            changed_at = status_line_html.css_first(".created_at time").attrs.get("datetime")
+        else:
+            changed_at = changed_at_html.attrs.get("datetime")
 
-            return CollectionCard(
-                title=html_tree.css_first(".l-page .head > h1").text(),
-                votes_for=int(votes_html.css_first(".votes-for").text()),
-                votes_against=int(votes_html.css_first(".votes-against").text()),
-                collection_size=int(status_line_html.css_first(".collection-size").text()),
-                comments_count=int(status_line_html.css_first(".comments").text()),
-                changed_at=changed_at
-            )
+        return CollectionCard(
+            title=html_tree.css_first(".l-page .head > h1").text(),
+            votes_for=int(votes_html.css_first(".votes-for").text()),
+            votes_against=int(votes_html.css_first(".votes-against").text()),
+            collection_size=int(status_line_html.css_first(".collection-size").text()),
+            comments_count=int(status_line_html.css_first(".comments").text()),
+            changed_at=changed_at
+        )
